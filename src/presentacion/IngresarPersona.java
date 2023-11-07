6ba5f644dde6ed871c73502823e4b3017d704ae0
Nicolas <nicolase.rubioc@konradlorenz.edu.co>
2023-11-07 15:35:56 -0500

    fff

--- src/presentacion/IngresarPersona.java
+++ src/presentacion/IngresarPersona.java
@@ -13,25 +13,38 @@ public class IngresarPersona extends JFrame{
     private JTextField txtApellido;
     private JLabel lblApellido;
     private JButton btnGuardar;
+    private JLabel lblIdioma;
+    private JComboBox cmbIdioma;
+    private JCheckBox chbTerminos;
+    private JLabel lblGenero;
+    private JRadioButton rbtMasculino;
+    private JRadioButton rbtFemenino;
+    private JRadioButton rbtNobinario;
+    private JRadioButton rbtPrefieronodecir;
+    private JComboBox Idiomas;
+
     public IngresarPersona () {
         this.setTitle("Ventana Ingresar Persona");
         this.setContentPane(plnPirncipal);
         this.setVisible(true);
-        this.setSize(400,300);
+        this.setSize(700,500);
         btnGuardar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String codigo = txtCodigo.getText();
                 String nombre = txtNombre.getText();
                 String apellido = txtApellido.getText();
-
-
-                String mensaje = "tus datos son: \n" +
+                String idioma = cmbIdioma.getSelectedItem().toString();
+                String aceptoTerminos = (chbTerminos.isSelected()) ? "Si" : "No";
+                String genero = (rbtMasculino.isSelected()) ? "No indicado" : "";
+                String mensaje = "Tus datos son: \n" +
                                  " Codigo: " + codigo + "\n" +
                                  "Nombre: " + nombre + "\n" +
-                                 "Apelldio: " + apellido + "\n" ;
-
-                JOptionPane.showMessageDialog(btnGuardar, "Guardado con exito");
+                                 "Apelldio: " + apellido + "\n" +
+                                 "Idioma: " + idioma + "\n" +
+                                 "Genero: " + genero + "\n" +
+                                 "Acepto Terminos: " + aceptoTerminos;
+                JOptionPane.showMessageDialog(btnGuardar, mensaje);
             }
         });
     }
